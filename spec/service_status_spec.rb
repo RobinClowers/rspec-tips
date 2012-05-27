5373e9bad800136dab85d08e2815ef53a5218675
Robin Clowers <robin.clowers@gmail.com>
2012-05-27 05:35:42 -0700

    add subject

--- spec/service_status_spec.rb
+++ spec/service_status_spec.rb
@@ -2,17 +2,21 @@ require 'rspec'
 require 'app/service_status'
 
 describe ServiceStatus do
+  subject { ServiceStatus.new status }
+
   context "when status is ok" do
+    let(:status) { 'ok' }
+
     it "service is available" do
-      status = ServiceStatus.new 'ok'
-      status.available?.should be_true
+      subject.available?.should be_true
     end
   end
 
   context "when status is down" do
+    let(:status) { 'down' }
+
     it "service is not available" do
-      status = ServiceStatus.new 'down'
-      status.available?.should be_false
+      subject.available?.should be_false
     end
   end
 end
