bbc1ed99fefe39e541d293349c39219c944770f3
Robin Clowers <robin.clowers@gmail.com>
2012-05-27 06:14:30 -0700

    add rails app

--- Gemfile
+++ Gemfile
@@ -1,5 +1,19 @@
-# A sample Gemfile
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "rspec"
-gem "factory_girl"
+gem 'rails', '3.2.2'
+gem 'sqlite3'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecTips::Application.config.secret_token = '0d45431f21e1e91cb91ec7e44e19a7f8a5ce2a4a1cba3d9075f4230065d839c5e40e69052e609eb6298e5b2310d4afef282da314520eee927241ca806a58de1c'
